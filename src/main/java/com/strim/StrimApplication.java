7df2685a251009d6c0c34f0277b145e11f580f2d
Juho <orot1195@gmail.com>
2025-07-15 22:43:32 +0900

    first commit

---
+++ src/main/java/com/strim/StrimApplication.java
@@ -0,0 +1,13 @@
+package com.strim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strim/StrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
